04fc77bdc476db44e13481e13cf7bdc97c9ab851
Simiyon Fyuba Tamang <st276419@my.ncwu.edu>
2025-01-02 23:18:17 -0500

    upload to imageKit functionality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -7,18 +7,20 @@ import useListenMessages from "../../hooks/useListenMessages";
 export const Messages = () => {
   const { messages, loading } = useGetMessages();
   useListenMessages();
-  const containerRef = useRef(null);
+  const lastMessageRef = useRef(null);
   useEffect(() => {
     setTimeout(() => {
-      containerRef.current?.scrollIntoView({ behavior: "smooth" });
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
     }, 100);
   }, [messages]);
   return (
-    <div className="px-4 flex-1 overflow-auto" ref={containerRef}>
+    <div className="px-4 flex-1 overflow-auto">
       {!loading &&
         messages.length > 0 &&
         messages.map((message) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
         ))}
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
